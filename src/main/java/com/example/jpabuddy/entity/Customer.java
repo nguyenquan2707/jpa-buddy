3eff3628e5e72dfa31f6dc0d3d05b3e7630b6126
Quan Nguyen <vozermuonnam1@gmail.com>
2023-11-28 21:17:15 +0700

    create entity and relation

---
+++ src/main/java/com/example/jpabuddy/entity/Customer.java
@@ -0,0 +1,31 @@
+package com.example.jpabuddy.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "customer")
+public class Customer {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "email")
+    private String email;
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "customer_id")
+    private Set<Product> products = new LinkedHashSet<>();
+
+}
---
+++ src/main/java/com/example/jpabuddy/entity/Product.java
@@ -0,0 +1,25 @@
+package com.example.jpabuddy.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "product")
+public class Product {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "quantity")
+    private Integer quantity;
+
+    @Column(name = "price")
+    private Long price;
+
+}
